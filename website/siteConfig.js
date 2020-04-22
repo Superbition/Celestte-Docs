25be85f103f2fd90236014be56ef264e3a93d073
embluk <embluk@tuta.io>
2020-04-22 15:57:18 +0100

    Change home link label

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -36,7 +36,7 @@ const siteConfig = {
     //{doc: 'doc4', label: 'API'},
     //{page: 'help', label: 'Help'},
     //{blog: true, label: 'Blog'},
-    {href: 'https://polyel.io', label: 'Home'}
+    {href: 'https://polyel.io', label: 'Polyel.io'}
   ],
 
   // If you have users set above, you add it here:
