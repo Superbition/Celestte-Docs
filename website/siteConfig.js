3e250ade7ee9a8c3a680b9b3ea8d937f79c5f8fc
embluk <embluk@tuta.io>
2020-07-14 00:03:47 +0100

    Disable last updated time

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -97,7 +97,7 @@ const siteConfig = {
   enableUpdateBy: false,
 
   // Show documentation's last update time.
-  enableUpdateTime: true,
+  enableUpdateTime: false,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
