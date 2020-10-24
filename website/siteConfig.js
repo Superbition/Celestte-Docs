e42c2a326d1bd6d22a695042eacc2a570f219341
embluk <embluk@tuta.io>
2020-10-24 23:08:30 +0100

    Add ending slashes on site URL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,8 +16,8 @@ const users = [
 const siteConfig = {
   title: 'Polyel PHP Framework', // Title for your website.
   tagline: 'A full stack asynchronous PHP framework',
-  url: 'https://polyel.io/docs', // Your website URL
-  baseUrl: '/docs', // Base URL for your project */
+  url: 'https://polyel.io/docs/', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://polyel.github.io',
   //   baseUrl: '/test-site/',
